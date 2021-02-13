9dc80d8d5e752aa43fb887dbc1b09328259746b8
fyz <121423428@qq.com>
2021-02-13 17:28:35 +0800

    在dev分支上添加adress

--- hello/src/com/fyz/test/Show.java
+++ hello/src/com/fyz/test/Show.java
@@ -5,9 +5,12 @@ public class Show {
 
         String name = "高圆圆";
         int age = 41;
+        String address = "深圳";
+
 
         System.out.println("name:"+name);
         System.out.println("age:"+age);
+        System.out.println("address:"+address);
     }
 }
 
