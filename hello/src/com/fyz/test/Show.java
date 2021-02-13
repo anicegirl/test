12e98a379e8e4ec068bce8d2dfaa9f3888200065
fyz <121423428@qq.com>
2021-02-13 17:17:07 +0800

    添加了age

--- hello/src/com/fyz/test/Show.java
+++ hello/src/com/fyz/test/Show.java
@@ -2,6 +2,8 @@ package com.fyz.test;
 
 public class Show {
     public static void main(String[] args) {
-        System.out.println("show修改了");
+
+        int age = 41;
+        System.out.println("age:"+age);
     }
 }
